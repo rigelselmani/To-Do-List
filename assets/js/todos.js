655c16e60d73917093e151283ff03c75c597d16a
rigel selmani <rigelrigelselmani@hotmail.com>
2019-07-09 19:26:01 -0400

    click functions

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,20 @@
 //Check off Specific Todos By Clicking
-$("li").click(function(){
+$("ul").on("click","li", function(){
  $(this).toggleClass("completed");
+});
+
+//click on X to delete Todo
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+   if(event.which===13){
+       var todoText= $(this).val();
+       $(this).val("");
+       $("ul").append("<li><span>X</span> "+todoText+"</li>")
+   }
 });
